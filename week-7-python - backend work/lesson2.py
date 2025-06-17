293fe40d7a4bf8cffa167ce328547d6ad5aecee0
2506-sine <sinelihlek@gmail.com>
2025-06-17 10:13:11 +0200

    week 6 - contact book

---
+++ week-7-python - backend work/lesson2.py
@@ -0,0 +1,35 @@
+first_name = "John"
+last_name = "Smith"
+city = 'New York'
+greeting = "Welcome to Python!"
+
+print(first_name)
+print(last_name)
+print(city)
+print(greeting)
+
+# When to use single vs double quotes:
+sentence = 'She said,"Python is great!"'
+print(sentence)
+
+contraction = "It's a sunny day."
+print(contraction)
+
+# multiline strings
+story = """Once upon a time,
+there was a programmer who loved PYthon."""
+# print(story)
+
+# String length
+word = "Python"
+#print(len(word))
+
+message = "Hello, World!"
+#print(len(message))
+
+# Acessing characters in a string
+new_word = "Python"
+print(new_word[0]) # Output:P
+print(new_word[3]) # Output:h
+
+print(new_word[1:3]) # Output:yt
